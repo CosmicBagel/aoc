b62748319019dfce077cdc49a51f0719756e24ce
CosmicBagel <>
2023-12-01 23:02:27 -0700

    setup for day3

---
+++ day3/day3.go
@@ -0,0 +1,9 @@
+package adventofcode2023golang
+
+func Day3P1() {
+	println("day 3 p 1")
+}
+
+func Day3P2() {
+	println("day 3 p 2")
+}
---
+++ day3/day3_test.go
@@ -0,0 +1,13 @@
+package adventofcode2023golang
+
+import(
+    "testing"
+)
+
+func TestDay3P1(t *testing.T) {
+    Day3P1()
+}
+
+func TestDay3P2(t *testing.T) {
+    Day3P2()
+}
