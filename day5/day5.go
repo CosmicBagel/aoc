7f3db635858dc2019985ca2d6d20197d36846609
CosmicBagel <>
2023-12-04 22:54:53 -0700

    partial work on day 5 p1 (not complete)

--- day5/day5.go
+++ day5/day5.go
@@ -5,6 +5,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+	maps defined by three numbers
+	destination start; source start; range
+*/
+
+type IdMap struct {
+	destination int
+	source      int
+	spread      int // range
+}
+
+type DataSet int
+
+const (
+	seedToSoil DataSet = iota
+	soilToFertilizer
+	fertilizerToWater
+	waterToLight
+	lightToTemperature
+	temperatureToHumidity
+	humidityToLocation
 )
 
 func Day5P1() {
@@ -20,11 +45,89 @@ func Day5P1() {
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		s := scanner.Text()
-		fmt.Println(s)
+	seeds, dataMap := parseInput(scanner)
+
+	// process to lowest location that corresponds with *any* of the seed numbers
+
+	fmt.Printf("%+v\n%+v\n", seeds, dataMap)
+}
+
+func parseInput(scanner *bufio.Scanner) ([]int, map[DataSet][]IdMap) {
+	dataMap := make(map[DataSet][]IdMap)
+	seeds := make([]int, 0)
+
+	headingToDataSet := func(s string) DataSet {
+		var d DataSet
+		switch s {
+		case "seed-to-soil":
+			d = seedToSoil
+		case "soil-to-fertilizer":
+			d = soilToFertilizer
+		case "fertilizer-to-water":
+			d = fertilizerToWater
+		case "water-to-light":
+			d = waterToLight
+		case "light-to-temperature":
+			d = lightToTemperature
+		case "temperature-to-humidity":
+			d = temperatureToHumidity
+		case "humidity-to-location":
+			d = humidityToLocation
+		default:
+			d = seedToSoil
+		}
+		return d
 	}
 
+	scanner.Scan()
+	firstLine := scanner.Text()
+	firstLineSplit := strings.Split(firstLine, ": ")
+	seedSplit := strings.Split(firstLineSplit[1], " ")
+	for _, s := range seedSplit {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatal(err)
+		}
+		seeds = append(seeds, num)
+	}
+
+	var currentDataSet DataSet
+	headerMode := false
+	for scanner.Scan() {
+		s := scanner.Text()
+		if len(s) == 0 {
+			headerMode = true
+			continue
+		}
+
+		if headerMode {
+			sSplit := strings.Split(s, " ")
+			header := sSplit[0]
+			currentDataSet = headingToDataSet(header)
+			headerMode = false
+			continue
+		}
+
+		//not header mode
+		sSplit := strings.Split(s, " ")
+		nums := make([]int, 0)
+		for _, numStr := range sSplit {
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				log.Fatal(err)
+			}
+			nums = append(nums, num)
+		}
+
+		var idMap = IdMap{nums[0], nums[1], nums[2]}
+
+		if dataMap[currentDataSet] == nil {
+			dataMap[currentDataSet] = make([]IdMap, 0)
+		}
+		dataMap[currentDataSet] = append(dataMap[currentDataSet], idMap)
+	}
+
+	return seeds, dataMap
 }
 
 func Day5P2() {
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -8,6 +8,6 @@ func TestDay5P1(t *testing.T) {
     Day5P1()
 }
 
-func TestDay5P2(t *testing.T) {
-    Day5P2()
-}
+// func TestDay5P2(t *testing.T) {
+    // Day5P2()
+// }
