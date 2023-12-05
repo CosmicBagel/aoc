525a24e64bca332e9f1e0f21bbdd4a3799823611
CosmicBagel <>
2023-12-05 00:07:10 -0700

    Partial progress towards day 5 p2

--- day5/day5_test.go
+++ day5/day5_test.go
@@ -4,10 +4,10 @@ import(
     "testing"
 )
 
-func TestDay5P1(t *testing.T) {
-    Day5P1()
-}
-
-// func TestDay5P2(t *testing.T) {
-    // Day5P2()
+// func TestDay5P1(t *testing.T) {
+//     Day5P1()
 // }
+
+func TestDay5P2(t *testing.T) {
+    Day5P2()
+}
