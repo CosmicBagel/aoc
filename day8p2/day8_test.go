7a2e06c4925ba0319b5e8f63ffd5cc193abf11bd
CosmicBagel <>
2023-12-08 03:52:21 -0700

    Finished day 8

--- day8p2/day8_test.go
+++
@@ -1,9 +0,0 @@
-package day8
-
-import(
-    "testing"
-)
-
-func TestDay8P2(t *testing.T) {
-    Day8P2()
-}
--- day8p2/day8p2.go
+++ day8p2/day8p2.go
@@ -1,4 +1,4 @@
-package day8
+package main
 
 import (
 	"bufio"
@@ -30,12 +30,12 @@ type Node struct {
 	right    *Node
 }
 
-func Day8P2() {
+func main() {
 	fmt.Println("day 8 p 2")
 	// file_name := "example_inputA.txt" //should require 2 steps
-	// file_name := "example_inputB.txt" //should require 6 steps
+	// file_name := "example_inputB.txt" //should require 5 steps
 	// file_name := "example_inputC.txt" //should require 6 steps
-	file_name := "input.txt"
+	file_name := "input.txt" // probable answer 8,811,050,362,409
 
 	file, err := os.Open(file_name)
 	if err != nil {
@@ -58,24 +58,29 @@ func Day8P2() {
 
 	//all following lines are the nodes
 	currentNodes := parseNodesP2(scanner)
+	nodeCount := len(currentNodes)
+	// fmt.Printf("starting node count %d\n", nodeCount)
 
 	//follow instructions along graph starting from root node
-	count := 0
+	var count uint64 = 0
 	instructionCount := len(instructions)
 	instructionPos := 0
 
-	areNodesDone := func() bool {
-		done := true
-		for _, n := range currentNodes {
-			if n.category != End {
-				done = false
-				break
-			}
-		}
-		return done
+	endReachedCounts := make([]int, nodeCount)
+
+	isCountingCycle := make([]bool, nodeCount)
+	hasCountedCycle := make([]bool, nodeCount)
+	endCycleCount := make([]uint64, nodeCount)
+	for i := range currentNodes {
+		isCountingCycle[i] = false
+		hasCountedCycle[i] = false
 	}
 
-	for !areNodesDone() {
+	// fmt.Println("starting path tracing")
+	for {
+		// allDone := true
+		allHasCounted := true
+
 		ins := instructions[instructionPos]
 		for i, node := range currentNodes {
 			switch ins {
@@ -84,12 +89,59 @@ func Day8P2() {
 			case R:
 				currentNodes[i] = node.right
 			}
+
+			if isCountingCycle[i] {
+				endCycleCount[i] += 1
+			}
+			if currentNodes[i].category != End {
+				// allDone = false
+			} else {
+				endReachedCounts[i] += 1
+
+				if !hasCountedCycle[i] {
+					hasCountedCycle[i] = true
+					endCycleCount[i] = count + 1
+
+					// fmt.Printf("%d cycle count %d\n", i, endCycleCount[i])
+					// fmt.Printf("\tend reached count %d\n", endReachedCounts[i])
+					// fmt.Printf("\tstep count %d\n", count+1)
+					// fmt.Printf("\tstep count - endCycleCount %d\n", (count+1)-uint64(endCycleCount[i]))
+				}
+
+				// if !isCountingCycle[i] {
+				// 	if !hasCountedCycle[i] {
+				// 		isCountingCycle[i] = true
+				// 		endCycleCount[i] = 0
+				// 		fmt.Printf("%d: starting cycle counting at steps %d\n", i, count + 1)
+				// 	}
+				// } else {
+				// 	isCountingCycle[i] = false
+				// 	hasCountedCycle[i] = true
+				// 	fmt.Printf("%d cycle count %d\n", i, endCycleCount[i])
+				// 	fmt.Printf("\tend reached count %d\n", endReachedCounts[i])
+				// 	fmt.Printf("\tstep count %d\n", count)
+				// 	fmt.Printf("\tstep count - endCycleCount %d\n", count-uint64(endCycleCount[i]))
+				// }
+
+			}
+			if !hasCountedCycle[i] {
+				allHasCounted = false
+			}
 		}
+
 		count += 1
 		instructionPos = (instructionPos + 1) % instructionCount
+
+		// if allDone || allHasCounted {
+		if allHasCounted {
+			break
+		}
 	}
 
-	fmt.Printf("step count: %d\n", count)
+	// fmt.Printf("using cycle counts to find least common multiple\n")
+	lcmResult := lowestCommonMultiple(endCycleCount)
+
+	fmt.Printf("total steps needed: %d\n", lcmResult)
 }
 
 func parseInstructionsP2(firstLine string) []Direction {
@@ -163,4 +215,74 @@ func parseNodesP2(scanner *bufio.Scanner) []*Node {
 	// }
 
 	return startingNodes
+}
+
+func lowestCommonMultiple(nums []uint64) uint64 {
+
+	// all primes below 1000
+	primes := []uint64{
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+		101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+		211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293,
+		307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397,
+		401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499,
+		503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599,
+		601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691,
+		701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+		809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887,
+		907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997,
+	}
+
+	factors := make([]map[uint64]uint64, len(nums))
+	for i := 0; i < len(factors); i++ {
+		factors[i] = make(map[uint64]uint64)
+	}
+	for i, n := range nums {
+		//find factors of n
+		f := factors[i]
+		for n > 1 {
+			noFactorFound := true
+			for _, p := range primes {
+				if n % p == 0 {
+					_, ok := f[p]
+					if !ok {
+						f[p] = 0
+					}
+					f[p] += 1
+					n /= p
+					noFactorFound = false
+					break
+				}
+			}
+			if noFactorFound {
+				log.Fatalf("Failed to find factor for %d\n", n)
+			}
+		}
+	}
+
+	// lcm factors are the factors of which have the highest count
+	// https://www.calculatorsoup.com/calculators/math/lcm.php
+	// LCM by prime factorization
+	lcmFactors := make(map[uint64]uint64)
+	for _, f := range factors {
+		for prime := range f {
+			_, ok := lcmFactors[prime]
+			if !ok {
+				lcmFactors[prime]  = 0
+			}
+			
+			if f[prime] > lcmFactors[prime] {
+				lcmFactors[prime] = f[prime]
+			}
+		}
+	}
+
+	var lcmResult uint64 = 1 
+	for prime := range lcmFactors {
+		for i := uint64(0); i < lcmFactors[prime]; i++ {
+			lcmResult *= uint64(prime)
+		}
+	}
+
+	return lcmResult
 }
