da52bd8c542c1f2d71467bcdaba01b6be5a90d56
CosmicBagel <>
2023-12-10 16:46:12 -0700

    day 9 possible working parser / graph builder

--- day10p1/day10p1.go
+++ day10p1/day10p1.go
@@ -17,13 +17,42 @@ build graph
 	also store original node character (for debug)
 	return starting node
 
-	- note connections can only be made if both points have mutual open side 
-		eg -| no connection 
+	- note connections can only be made if both points have mutual open side
+		eg -| no connection
 		   -7 connection
 
 trace from start forwards and backwards, where the two traveling pointers meet
 will be the furthest distance (only following prev pointers, one following next pointers)
 */
+
+type Point struct {
+	X int
+	Y int
+}
+
+type NodeType int
+
+const (
+	Empty NodeType = iota
+	Vertical
+	Horizontal
+	NEBend
+	NWBend
+	SWBend
+	SEBend
+	Start
+)
+
+type Node struct {
+	north *Node
+	east  *Node
+	south *Node
+	west  *Node
+
+	nodeType       NodeType
+	location       Point
+	originalMarker rune
+}
 
 func main() {
 	fmt.Println("day 10 p 1")
@@ -40,6 +69,126 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	// scanner.Scan()
+	// fmt.Println(scanner.Text())
+	startingNode := parse(scanner)
+	result := findFarthestDistance(startingNode)
+
+	fmt.Printf("fartherst distance: %d", result)
+}
+
+func parse(scanner *bufio.Scanner) Node {
+	startingNode := Node{nodeType: Empty}
+
 	scanner.Scan()
-	fmt.Println(scanner.Text())
+	currentLine := scanner.Text()
+	gridWidth := len(currentLine)
+
+	aboveNodes := make([]Node, 0, gridWidth)
+	currentNodes := make([]Node, 0, gridWidth)
+
+	for i := 0; i < gridWidth; i++ {
+		n := Node{
+			location:       Point{i, -1},
+			originalMarker: '.',
+			nodeType:       Empty,
+		}
+		aboveNodes = append(aboveNodes, n)
+	}
+
+	y := 0
+	for {
+		for x, c := range currentLine {
+
+			loc := Point{x, y}
+			aboveNode := aboveNodes[x]
+			leftNode := Node{nodeType: Empty}
+			if x > 0 {
+				leftNode = currentNodes[x-1]
+			}
+
+			n := Node{
+				location:       loc,
+				originalMarker: c,
+				nodeType:       Empty,
+			}
+
+			ant := aboveNode.nodeType
+			aboveHasSouth := ant == Vertical || ant == SEBend || ant == SWBend || ant == Start
+			lnt := leftNode.nodeType
+			leftHasEast := lnt == Horizontal || lnt == NEBend || lnt == SEBend || lnt == Start
+
+			switch c {
+			case '|':
+				n.nodeType = Vertical
+				if aboveHasSouth {
+					n.north = &aboveNode
+					aboveNode.south = &n
+				}
+			case '-':
+				n.nodeType = Horizontal
+				if leftHasEast {
+					n.west = &n
+					leftNode.east = &n
+				}
+			case 'L':
+				n.nodeType = NEBend
+				if aboveHasSouth {
+					n.north = &aboveNode
+					aboveNode.south = &n
+				}
+			case 'J':
+				n.nodeType = NWBend
+				if leftHasEast {
+					n.west = &n
+					leftNode.east = &n
+				}
+				if aboveHasSouth {
+					n.north = &aboveNode
+					aboveNode.south = &n
+				}
+			case '7':
+				n.nodeType = SWBend
+				if leftHasEast {
+					n.west = &n
+					leftNode.east = &n
+				}
+			case 'F':
+				n.nodeType = SEBend
+			case 'S':
+				n.nodeType = Start
+				startingNode = n
+				if leftHasEast {
+					n.west = &n
+					leftNode.east = &n
+				}
+				if aboveHasSouth {
+					n.north = &aboveNode
+					aboveNode.south = &n
+				}
+			}
+
+			currentNodes = append(currentNodes, n)
+		}
+
+		// clear and swap lines
+		clear(aboveNodes)
+		temp := aboveNodes
+		aboveNodes = currentNodes
+		currentNodes = temp
+
+		y += 1
+		if !scanner.Scan() {
+			break
+		}
+	}
+
+	if startingNode.nodeType == Empty {
+		log.Fatal("did not find starting node")
+	}
+	return startingNode
+}
+
+func findFarthestDistance(startingNode Node) int {
+	return 0
 }
