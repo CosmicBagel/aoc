4211feab371b9dccfa110db6ddffc81578aff76f
CosmicBagel <>
2024-07-07 20:48:53 -0600

    Finihsed day 3

--- day03/main.zig
+++ day03/main.zig
@@ -12,11 +12,13 @@ pub fn main() !void {
     // hashset for visited coordinates
     // bump 'visted' count when new entry is made
 
-    var x: i32 = 0;
-    var y: i32 = 0;
+    const Coord = struct { x: i32, y: i32 };
+
+    var santa = Coord{ .x = 0, .y = 0 };
+    var robot = Coord{ .x = 0, .y = 0 };
+    var flipFlop = true;
     var visitedCount: u32 = 0;
 
-    const Coord = struct { x: i32, y: i32 };
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var visitedMap = std.AutoHashMap(Coord, void).init(
         gpa.allocator(),
@@ -33,23 +35,25 @@ pub fn main() !void {
             else => |e| return e,
         };
 
+        var coord = if (flipFlop) &santa else &robot;
+        flipFlop = !flipFlop;
+
         switch (byte) {
-            '>' => x += 1,
-            '<' => x -= 1,
-            '^' => y += 1,
-            'v' => y -= 1,
+            '>' => coord.x += 1,
+            '<' => coord.x -= 1,
+            '^' => coord.y += 1,
+            'v' => coord.y -= 1,
             '\n' => {},
             else => {
-                std.debug.print("surprise character '{c}', 0x{x}\n", .{byte, byte});
+                std.debug.print("surprise character '{c}', 0x{x}\n", .{ byte, byte });
                 return error.Oops;
             },
         }
 
-        const coord = Coord{ .x = x, .y = y };
-        const contains = visitedMap.contains(coord);
+        const contains = visitedMap.contains(coord.*);
         if (!contains) {
             visitedCount += 1;
-            try visitedMap.put(coord, {});
+            try visitedMap.put(coord.*, {});
         }
     }
 
